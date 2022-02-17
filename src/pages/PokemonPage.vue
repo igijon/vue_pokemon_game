6137d2395f79c206279d5b1748257dfa21636bfb
Inma Gijón <igijoninf@gmail.com>
2022-02-17 21:25:38 +0100

    Primeros componentes

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,14 +1,19 @@
 <template>
   <h1>¿Quién es este Pokémon?</h1>
-  <!--TODO: Picture -->
 
-  <!-- TODO: Options-->
+  <PokemonPicture/>
+  <PokemonOptions/>
 </template>
 
 <script>
+import PokemonOptions from '../components/PokemonOptions.vue'
+//import PokemonOptions from '@/components/PokemonOptions.vue'. @ parte de src.
+import PokemonPicture from '../components/PokemonPicture.vue'
 export default {
     name: 'PokemonPage',
     components: {
+        PokemonOptions,
+        PokemonPicture
     }
 }
 </script>
