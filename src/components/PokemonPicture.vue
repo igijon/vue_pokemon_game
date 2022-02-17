5324bea4c6a0a6c5fda5b7c599e84f6a17c14089
Inma Gijón <igijoninf@gmail.com>
2022-02-17 21:32:20 +0100

    Manipulación de imagenes

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,13 +1,24 @@
 <template>
-  <h3>Pokemon picture</h3>
+  <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+    class="hidden-pokemon"
+    alt="pokemon"
+  />
+  <!--Si hacemos drag sería una pista del pokemon que hay-->
+  <img
+    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+    class="fade-in" 
+    alt="pokemon"
+  />
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<!-- Para que sólo se aplique a un componente tenemos que poner scoped-->
+<style scoped> 
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
