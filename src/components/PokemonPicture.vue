58ccd93a00efab5d985907e2ee87a13ce589e9a7
Inma Gijón <igijoninf@gmail.com>
2022-02-19 11:40:13 +0100

    Propiedad pokemonId y propiedad computada

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="pokemon-container">
     <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      :src="imgSrc"
       class="hidden-pokemon"
       alt="pokemon"
     />
     <!-- <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      :src="imgSrc"
       class="fade-in"
       alt="pokemon"
     /> -->
@@ -14,8 +14,23 @@
 </template>
 
 <script>
-export default {};
+export default {
+
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ this.pokemonId }.svg`
+    }
+  }
+
+}
 </script>
+
 
 <!-- Para que sólo se aplique a un componente tenemos que poner scoped-->
 <style scoped>
