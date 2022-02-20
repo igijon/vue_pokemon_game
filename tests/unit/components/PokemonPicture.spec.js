3a4d578256a1c5a0de72b571f48fd8868c71cf09
Inma Gijón <igijoninf@gmail.com>
2022-02-20 18:56:52 +0100

    PokemonPicture tests: preparación pruebas

---
+++ tests/unit/components/PokemonPicture.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils'
+import PokemonPictureñ from '@/components/PokemonPicture'
+
+describe('PokemonPicture component', () => {
+
+    test('debe hacer match con snapshot', () => {
+
+
+    })
+
+    test('debe mostrar la imagen oculta y el pokemon 100', () => {
+
+    })
+
+    test('debe mostrar el pokemon si showPokemon está en true', () => {
+
+        
+    })
+})
