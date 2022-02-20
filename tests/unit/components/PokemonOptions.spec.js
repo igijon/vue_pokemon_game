6582f1e035d67a0e1454fb3381ba2222deca38d3
Inma Gijón <igijoninf@gmail.com>
2022-02-20 19:40:32 +0100

    PokemonOptions: match con snapshot y mock de pokemons

---
+++ tests/unit/components/PokemonOptions.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from '@vue/test-utils'
+import PokemonOptions from '@/components/PokemonOptions'
+
+import { pokemons } from '../mocks/pokemons.mock'
+//Con yarn test:unit /PokemonOptions sólo hace de ese componente, si no ponemos / ejecuta también 
+//los test de getPokemonOptions
+describe('PokemonOptions component', () => {
+
+    let wrapper 
+
+    beforeEach(() => {
+        wrapper = shallowMount( PokemonOptions , {
+            props: {
+                //pokemons:pokemon
+                pokemons
+            }
+        })
+    })
+
+    test('debe hacer match con snapshot', () => {
+        console.log( wrapper.html() )
+        expect( wrapper.html() ).toMatchSnapshot()
+    })
+
+
+})
