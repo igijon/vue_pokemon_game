87ce5e5557e9eb7e0bcf07e5ee1ee6e824ba9089
Inma Gijón <igijoninf@gmail.com>
2022-02-20 19:48:57 +0100

    Test sobre las cuatro opciones

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -5,29 +5,37 @@ import { pokemons } from "../mocks/pokemons.mock";
 //Con yarn test:unit /PokemonOptions sólo hace de ese componente, si no ponemos / ejecuta también
 //los test de getPokemonOptions
 describe("PokemonOptions component", () => {
-  let wrapper;
+    let wrapper
 
-  beforeEach(() => {
-    wrapper = shallowMount(PokemonOptions, {
-      props: {
-        //pokemons:pokemon
-        pokemons,
-      },
+    beforeEach(() => {
+        wrapper = shallowMount(PokemonOptions, {
+            props: {
+                //pokemons:pokemon
+                pokemons,
+            }
+        })
     })
-  })
 
-  test("debe hacer match con snapshot", () => {
+    test("debe hacer match con snapshot", () => {
     
-    expect(wrapper.html()).toMatchSnapshot()
-  })
+        expect(wrapper.html()).toMatchSnapshot()
+  
+    })
 
-  test('debe mostrar las cuatro opciones correctamente', () => {
+    test('debe mostrar las cuatro opciones correctamente', () => {
+        
+        const liTags = wrapper.findAll('li')
 
-  })
+        expect( liTags.length ).toBe(4)
+        expect( liTags[0].text() ).toBe('pikachu')
+        expect( liTags[1].text() ).toBe('charmander')
+        expect( liTags[2].text() ).toBe('venusaur')
+        expect( liTags[3].text() ).toBe('mew')
+    })
 
-  test('debe emitir "selection" con sus respectivos parámetros al hacer click', () => {
+    test('debe emitir "selection" con sus respectivos parámetros al hacer click', () => {
 
 
-  })
+    })
 
 })
