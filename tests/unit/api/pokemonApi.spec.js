d6407d9049278c5ddf39e04dffa6ddc1d69a4480
Inma Gijón <igijoninf@gmail.com>
2022-02-20 18:07:03 +0100

    test unit config axios

--- tests/unit/api/pokemonApi.spec.js
+++ tests/unit/api/pokemonApi.spec.js
@@ -1,7 +1,7 @@
 import pokemonApi from '@/api/pokemonApi'
 
 //Ejecutar tests con yarn test:unit
-
+//Comprobamos si nuestra isntancia de axios está bien configurada
 describe('pokemonApi', () => {
 
     test('axios debe estar configurado', () => {
