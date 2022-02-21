22bcd9485aaa23527962f2c40dc767bb54fc648e
Inma Gijón <igijoninf@gmail.com>
2022-02-21 10:58:21 +0100

    Espía fallido

--- tests/unit/pages/PokemonPage.spec.js
+++ tests/unit/pages/PokemonPage.spec.js
@@ -14,4 +14,15 @@ describe('PokemonPage component', () => {
     /*
     No merece la pena el match con el snapshot porque si no hay pokemon sólo renderiza el h2 de espere por favor
     */
+    test('debe hacer match con el snapshot', () => {
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    test('debe llamar mixPokemonArray al montar', () => {
+
+        //Cuando hago el espía, el componente está montado y ya se ha disparado
+        const mixPokemonArraySpy = jest.spyOn( PokemonPage.methods, 'mixPokemonArray' )
+        expect( mixPokemonArraySpy ).toHaveBeenCalled()
+
+    })
 })
